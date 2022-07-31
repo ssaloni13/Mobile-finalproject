9825507a0acf1d2f5f6bc1d54e0ce5ed2616183c
Shivam Shukla <shivam.p.shukla@medtronic.com>
2022-07-31 14:57:15 -0400

    Login Activity Layout Created

--- app/src/main/java/com/example/mobile_finalproject/LoginActivity.java
+++ app/src/main/java/com/example/mobile_finalproject/LoginActivity.java
@@ -2,17 +2,13 @@ package com.example.mobile_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(this, LoginActivity.class));
-        this.finish();
+        setContentView(R.layout.activity_login);
     }
 }
