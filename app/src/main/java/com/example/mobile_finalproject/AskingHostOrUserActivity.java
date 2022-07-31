98a03279273411b6d76fac216a1bfb1ff7fbadc2
Shivam Shukla <shivam.p.shukla@medtronic.com>
2022-07-31 16:55:14 -0400

    Created Host/User asking activity

---
+++ app/src/main/java/com/example/mobile_finalproject/AskingHostOrUserActivity.java
@@ -0,0 +1,36 @@
+package com.example.mobile_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AskingHostOrUserActivity extends AppCompatActivity {
+
+    private Button button_host, button_user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asking_host_or_user);
+
+        button_host = findViewById(R.id.button_host);
+        button_host.setOnClickListener(v -> openHostActivity());
+
+        button_user = findViewById(R.id.button_user);
+        button_user.setOnClickListener(v -> openUserActivity());
+    }
+
+    private void openHostActivity() {
+        // TODO Open Host activity and send user type: HOST
+        Intent intent = new Intent();
+        startActivity(intent);
+    }
+
+    private void openUserActivity() {
+        // TODO Open User activity and send user type: USER
+        Intent intent = new Intent();
+        startActivity(intent);
+    }
+}
