96106f58d4548c293fbc5881f9cf8ac1e7d213f0
Shivam Shukla <shivam.p.shukla@medtronic.com>
2022-08-07 13:58:04 -0400

    Changed splash screen timing to 3 seconds

--- app/src/main/java/com/example/mobile_finalproject/MainActivity.java
+++ app/src/main/java/com/example/mobile_finalproject/MainActivity.java
@@ -16,17 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This code snippet creates a splash screen effect for 5 seconds
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                final Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
-                MainActivity.this.startActivity(mainIntent);
-                MainActivity.this.finish();
-            }
-        }, 5000);
-
-        //startActivity(new Intent(this, AskingHostOrUserActivity.class));
-        //this.finish();
+        //This code snippet creates a splash screen effect for 3 seconds
+        new Handler().postDelayed(() -> {
+            final Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
+            MainActivity.this.startActivity(mainIntent);
+            MainActivity.this.finish();
+        }, 3000);
     }
 }
